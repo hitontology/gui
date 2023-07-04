d65838163ec3f57ad47f696390d5d24feec7028e
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-07-04 17:00:49 +0200

    even more results

--- js/result.js
+++ js/result.js
@@ -2,13 +2,21 @@ function search(query) {
   document.body.innerHTML += `<h2>Search results for: ${query}</h2>`;
 }
 
-function result(title, text)
-{
-		return 
+function result(title, text, link) {
+  return `<div class="result">
+          <h3>${title}</h3>
+          ${text}
+          <a href="${link}">More Info</a>
+        </div>`;
 }
 
-function results()
-{
+function results() {
+  const container = document.getElementById("results");
+  for (let i = 1; i <= 10; i++) {
+    let html = result("Commercial RIS " + i, "Commercial Radiology Information System (RIS) Dummy " + i, "");
+    let fragment = document.createRange().createContextualFragment(html);
+    container.appendChild(fragment);
+  }
 }
 
 document.addEventListener(
@@ -17,6 +25,7 @@ document.addEventListener(
     const urlParams = new URLSearchParams(window.location.search);
     const query = urlParams.get("query");
     search(query);
+    results();
   },
   false
 );
