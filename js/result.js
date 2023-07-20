0c5ca3e90d0c0751a8f47da3a35feee08c0b0820
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-07-20 15:28:54 +0200

    highlight elements after search

--- js/result.js
+++ js/result.js
@@ -5,31 +5,3 @@ function search(query) {
   document.getElementById("definition").innerText =
     "Das LIS unterstützt alle Schritte der Laboruntersuchung: die Verwaltung von Aufträgen und Proben, die Verteilung der Proben auf die vorhandenen Analysegeräte, das Abrufen und Validieren der Ergebnisse und die abschließende Übermittlung der Ergebnisse an den Auftraggeber. Dadurch wird der nahezu vollautomatische Laborbetrieb ermöglicht, welcher die hohen Durchsatzraten in modernen medizinischen Laborinstituten erlaubt. Analysegeräte können direkt an das Informationssystem angeschlossen werden, um die Werte direkt in das System zu übernehmen.";
 }
-
-function result(title, text, link) {
-  return `<div class="result">
-          <h3>${title}</h3>
-          ${text}
-          <a href="${link}">More Info</a>
-        </div>`;
-}
-
-function results() {
-  const container = document.getElementById("results");
-  for (let i = 1; i <= 10; i++) {
-    let html = result("Commercial RIS " + i, "Commercial Radiology Information System (RIS) Dummy " + i, "details.html?swp=Commercial RIS " + i);
-    let fragment = document.createRange().createContextualFragment(html);
-    container.appendChild(fragment);
-  }
-}
-
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const query = urlParams.get("query");
-    search(query);
-    results();
-  },
-  false
-);
---
+++ js/search.js
@@ -0,0 +1,10 @@
+function search(event) {
+  event.preventDefault();
+  document.getElementById("ast").classList.add("highlight");
+  document.getElementById("feature").classList.add("highlight");
+  document.getElementById("function").classList.add("highlight");
+  document.getElementById("orgunit").classList.add("highlight");
+  document.getElementById("usergroup").classList.add("highlight");
+  document.getElementById("swp").classList.add("highlight");
+  document.getElementById("study").classList.add("highlight");
+}
