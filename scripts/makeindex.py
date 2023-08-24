2d262b4bc7daafbb061004498484f376cbcfeb97
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-08-24 09:10:46 +0200

    improve makeindex.py

--- scripts/makeindex.py
+++ scripts/makeindex.py
@@ -7,11 +7,16 @@ import json
 
 sparql_query = """
 PREFIX hito: <http://hitontology.eu/ontology/>
-SELECT DISTINCT ?s (GROUP_CONCAT(STR(?sl); SEPARATOR="|") AS ?sls) (GROUP_CONCAT(STR(?comment); SEPARATOR="|") AS ?comments)
+SELECT DISTINCT ?s SAMPLE(?type)
+(GROUP_CONCAT(DISTINCT STR(?sl); SEPARATOR="|") AS ?sls)
+(GROUP_CONCAT(DISTINCT STR(?cl); SEPARATOR="|") AS ?cls)
+(GROUP_CONCAT(DISTINCT STR(?comment); SEPARATOR="|") AS ?comments)
 FROM <http://hitontology.eu/ontology>
 {
- ?s a [rdfs:subClassOf hito:Classified].
- ?s rdfs:label ?sl.
+ ?s a ?type.
+ ?type rdfs:subClassOf hito:Classified.
+ ?s rdfs:label|skos:altLabel ?sl.
+ OPTIONAL {?p rdfs:subPropertyOf hito:classified. ?cit ?p ?s. ?cit rdfs:label|skos:altLabel ?cl.}  
  OPTIONAL {?s rdfs:comment ?comment.}
 }
 GROUP BY ?s
@@ -24,14 +29,23 @@ full_url = base_url + encoded_query
 
 response = requests.get(full_url)
 output = response.text
+entries = []
 
-lines = output.strip().split("\n")
+# first line is table header
+lines = output.strip().split("\n")[1:]
 for line in lines:
     parts = line.split("\t")
-    if len(parts) == 3:
-        subject, labelstr, commentstr = [part.strip('"').strip() for part in parts]
-        labels = labelstr.split("|")
-        comments = commentstr.split("|")
-        #transformed_line = f"{subject} {labels} {comments}"
-        transformed_line = f"{subject} {labels}"
-        print(transformed_line)
+    if len(parts) == 5:
+        subject, type, labelstr, citationstr, commentstr  = [part.strip('"').strip() for part in parts]
+        # split converts "" to [""]
+        labels = labelstr.split("|") if labelstr else []
+        citations = citationstr.split("|") if citationstr else []
+        comments = commentstr.split("|") if commentstr else []
+        #transformed_line = f"{subject} {labels}"
+        #print(transformed_line)
+        entries.append({"s": subject, "t": type, "l": labels, 'cit': citations})# , "com": comments})
+
+filename = "index.json"
+with open(filename, 'w') as file:
+    file.write(json.dumps(entries).replace("]}, {","]},\n{"))
+print(f"{len(entries)} results written to {filename}")
