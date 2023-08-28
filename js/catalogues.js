4d074cc26fd9d8b10e994039feaa0f3c45148f9a
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-08-28 13:40:12 +0200

    more prototype

--- js/catalogues.js
+++ js/catalogues.js
@@ -1,25 +1,19 @@
-function result(title, text, link) {
-  return `<div class="result">
-          <img class="catalogueimg" src="img/catalogue.svg">
+function catalogue(title, text, link) {
+  return `<div class="box">
+          <img class="boximg" src="img/catalogue.svg">
           <h3>${title}</h3>
           ${text}
           <a href="${link}">More Info</a>
         </div>`;
 }
 
-function results() {
-  const container = document.getElementById("results");
+function catalogues() {
+  const container = document.getElementById("catalogues");
   for (let i = 1; i <= 10; i++) {
-    let html = result("Catalog" + i, "A collection of classified things " + i, "catalogue.html?cat=BbApplicationSystemTypeCatalogue");
+    let html = catalogue("Catalog" + i, "A collection of classified things " + i, "catalogue.html?cat=BbApplicationSystemTypeCatalogue");
     let fragment = document.createRange().createContextualFragment(html);
     container.appendChild(fragment);
   }
 }
 
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    results();
-  },
-  false
-);
+document.addEventListener("DOMContentLoaded", () => catalogues(), false);
