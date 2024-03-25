66b9d13be021edccee2f031e5162ab2378e52b2a
Hannes R. Brunsch <hrbrunsch@gmail.com>
2024-03-25 10:17:29 +0100

    fix: resolves #32

--- scripts/diagram-prepare.sh
+++ scripts/diagram-prepare.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # download the SVG image to img/diagram.svg
-wget -N https://hitontology.eu/public/2024-03-hito_diagram.svg -O img/diagram.svg
+wget https://hitontology.eu/public/2024-03-hito_diagram.svg -O img/diagram.svg
 
 # Preprocessing
 # 1. x,y,height,width 5.831902424 -> 5.0
@@ -14,7 +14,7 @@ sed -i -z -r \
 	-e 's/([xyth]\="[0-9]+)\.[0-9]+/\1.0/g' \
 	-e 's/(<path fill="none" d="[0-9A-Z. ]* L)[0-9.]+ [0-9.]+"(\/>[\r\n]+[[:blank:]]*<path )(d="M)([0-9.]+ [0-9.]+)/\1\4" class="arrow-body" \2class="arrow-head" \3\4/g' \
 	-e 's/<g id="y\.(node|edge)\.[0-9]{1,2}">[\r\n]+[[:blank:]]*<a target="_blank" xlink:type="simple" xlink:href="(https:\/\/hitontology.eu\/ontology\/)*([A-Za-z]+)" xlink:show="[a-zA-Z]+">[\r\n]+([0-9A-Za-z \t-.":(),<>=\r\n\/]+)([\r\n]+)[[:blank:]]+<\/a>/<g id="\3">\5\4/g' \
-	-e '1s/image-rendering="auto"/image-rendering="auto" viewbox="0 0 1431 574"/g' \
-	-e '4s/id="genericDefs"/id="genericDefs" y="5"/g' \
+	-e 's/width="([0-9]+)"([a-zA-Z0-9"'"'"' -=]+) height="([0-9]+)"/\2 viewbox="0 0 \1 \3" width="100%"/1' \
+	-e 's/id="genericDefs"/id="genericDefs" y="5"/1' \
 	-e 's/[\r\n]+[[:blank:]]*//g' \
 	img/diagram.svg
