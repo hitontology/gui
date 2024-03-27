3d8f872ef1aa67fdcd15e2cc1d248df6ad50387e
Hannes R. Brunsch <hrbrunsch@gmail.com>
2024-03-27 09:26:55 +0100

    feat: svg preprocessing explicitly differentiate nodes edges by adding a class

--- scripts/diagram-prepare.sh
+++ scripts/diagram-prepare.sh
@@ -16,7 +16,7 @@ echo "modifying $IMGPATH/diagram.svg..."
 sed -i -z -r \
 	-e 's/([xyth]\="[0-9]+)\.[0-9]+/\1.0/g' \
 	-e 's/(<path fill="none" d="[0-9A-Z. ]* L)[0-9.]+ [0-9.]+"(\/>[\r\n]+[[:blank:]]*<path )(d="M)([0-9.]+ [0-9.]+)/\1\4" class="arrow-body" \2class="arrow-head" \3\4/g' \
-	-e 's/<g id="y\.(node|edge)\.[0-9]{1,2}">[\r\n]+[[:blank:]]*<a target="_blank" xlink:type="simple" xlink:href="(https:\/\/hitontology.eu\/ontology\/)*([A-Za-z]+)" xlink:show="[a-zA-Z]+">[\r\n]+([0-9A-Za-z \t-.":(),<>=\r\n\/]+)([\r\n]+)[[:blank:]]+<\/a>/<g id="\3">\5\4/g' \
+	-e 's/<g id="y\.(node|edge)\.[0-9]{1,2}">[\r\n]+[[:blank:]]*<a target="_blank" xlink:type="simple" xlink:href="(https:\/\/hitontology.eu\/ontology\/)*([A-Za-z]+)" xlink:show="[a-zA-Z]+">[\r\n]+([0-9A-Za-z \t-.":(),<>=\r\n\/]+)([\r\n]+)[[:blank:]]+<\/a>/<g id="\3" class="\1">\5\4/g' \
 	-e 's/width="([0-9]+)"([a-zA-Z0-9"'"'"' -=]+) height="([0-9]+)"/\2 viewbox="0 0 \1 \3" width="100%"/1' \
 	-e 's/id="genericDefs"/id="genericDefs" y="5"/1' \
 	$IMGPATH/diagram.svg
